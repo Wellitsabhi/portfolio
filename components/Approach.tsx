e314372e39ab30dd687bf2fe451a6d5b92940649
wellitsabhi <abhishekongit@gmail.com>
2024-06-23 12:02:01 +0530

    fix: removed description

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -15,9 +15,10 @@ const Approach = () => {
         <Card
           title="Planning & Strategy"
           icon={<AceternityIcon order="Phase 1" />}
-          des="We'll collaborate to map out your website's goals, target audience, 
-          and key functionalities. We'll discuss things like site structure, 
-          navigation, and content requirements."
+          des=""
+          // des="We'll collaborate to map out your website's goals, target audience,
+          // and key functionalities. We'll discuss things like site structure,
+          // navigation, and content requirements."
         >
           <CanvasRevealEffect
             animationSpeed={5.1}
@@ -28,9 +29,10 @@ const Approach = () => {
         <Card
           title="Development & Progress Update"
           icon={<AceternityIcon order="Phase 2" />}
-          des="Once we agree on the plan, I cue my lofi playlist and dive into
-          coding. From initial sketches to polished code, I keep you updated
-          every step of the way."
+          des=""
+          // des="Once we agree on the plan, I cue my lofi playlist and dive into
+          // coding. From initial sketches to polished code, I keep you updated
+          // every step of the way."
         >
           <CanvasRevealEffect
             animationSpeed={3}
@@ -50,6 +52,7 @@ const Approach = () => {
         <Card
           title="Development & Launch"
           icon={<AceternityIcon order="Phase 3" />}
+          des=""
           des="This is where the magic happens! Based on the approved design, 
           I'll translate everything into functional code, building your website
           from the ground up."
